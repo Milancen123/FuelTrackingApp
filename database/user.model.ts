9047db03c7d9c9fd3e262e05db1a0d9110a0403a
Milan <milan.milancen12345@gmail.com>
2025-10-10 08:32:52 +0200

    User model created

---
+++ database/user.model.ts
@@ -0,0 +1,24 @@
+import { model, models, Schema } from "mongoose";
+
+
+export interface IUser{
+    name:string;
+    username:string;
+    email:string;
+    image:string;
+}
+
+const UserSchema = new Schema({
+    name: {type:String, required:true},
+    username: {type:String, required:true},
+    email:{type:String, required:true, unique:true},
+    image: {type:String, required:true},
+}, {
+    timestamps:true
+});
+
+
+const User = models?.user || model("User", UserSchema);
+
+export default User;
+
